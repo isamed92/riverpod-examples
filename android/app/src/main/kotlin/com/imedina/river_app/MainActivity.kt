425e9c7d7633aadce7dc8d9d3f5b3e19fafab0e4
Isaias Medina <isamed92@gmail.com>
2023-06-14 03:13:07 -0300

    initial commit: add router, theme, screen

---
+++ android/app/src/main/kotlin/com/imedina/river_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.imedina.river_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,13 @@
+import 'package:go_router/go_router.dart';
+import 'package:river_app/presentation/screens/screens.dart';
+
+final appRouter = GoRouter(
+    // initialLocation: '/',
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) {
+          return const HomeScreen();
+        },
+      )
+    ]);
